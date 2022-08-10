319f0db50bc342c63443321c635d2cc0bc2c2fe3
ChronoLogic <msuttie12@hotmail.com>
2022-08-10 22:09:25 +0100

    Create file structure

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import * as Pages from './pages';
+import React from 'react';
+import { Routes, Route } from 'react-router-dom';
+import './styles/styles.css';
+
+function App() {
+	return (
+		<Routes>
+			<Route exact path="/" element={<Pages.Landing />} />
+			<Route path="*" element={<Pages.NotFound />} />
+		</Routes>
+	);
+}
+
+export default App;
---
+++ src/pages/Landing/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+export default () => {
+	return (
+		<>
+			<main>
+				<h1>Hello World!</h1>
+			</main>
+		</>
+	);
+};
